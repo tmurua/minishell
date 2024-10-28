3046568b8ac903ea3622694d47bf5d6a2e32cd4c
maricalmer <maricalmer@proton.me>
2024-10-28 12:17:37 +0100

    feat: include header to main.c

--- main.c
+++ main.c
@@ -6,9 +6,11 @@
 /*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:19:43 by dlemaire          #+#    #+#             */
-/*   Updated: 2024/10/28 12:02:43 by tmurua           ###   ########.fr       */
+/*   Updated: 2024/10/28 12:16:39 by dlemaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "minishell.h"
 
 int	main(int ac, char *av[], char *envp[])
 {
