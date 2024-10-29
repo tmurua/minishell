a28908eb1d06d76814e11c8a1640d9f46848e234
maricalmer <maricalmer@proton.me>
2024-10-29 17:49:12 +0100

    feat: create ast.c

---
+++ ast.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlemaire <dlemaire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 17:10:48 by dlemaire          #+#    #+#             */
+/*   Updated: 2024/10/29 17:48:42 by dlemaire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// AST: hierarchical syntactic structure of the source program
+
+// Any programming construct can be
+// handled by making up an operator for the construct and treating as operands
+// the semantically meaningful components of that construct
+
+// In an abstract syntax tree for an expression, each
+// interior node represents an operator. 
+
