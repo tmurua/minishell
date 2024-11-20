70f709fc028176f51d244ba01d9804454a768c06
Tom Murua <tommurua@gmail.com>
2024-11-20 15:25:48 +0100

    fix: remove head for garbage_collector t_list for now

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 11:19:43 by dlemaire          #+#    #+#             */
-/*   Updated: 2024/11/20 13:17:37 by tmurua           ###   ########.fr       */
+/*   Updated: 2024/11/20 15:24:26 by tmurua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 int	main(int ac, char *av[], char *envp[])
 {
 	char	**env;
-	t_list	*gc;
 
-	gc = NULL;
 	setup_prompt_signals();
 	env = duplicate_env(envp);
 	if (!env)
